0da63a2e6133e4f6431d35cd0a78aa1690b8e7ed
Ritham Sheti <sethiritham@gmail.com>
2026-01-24 10:21:39 +0530

    feat : created a simple opGL, glfw render window

--- GLFW_example.cpp
+++ GLFW_example.cpp
@@ -1,11 +1,26 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+
 
 void dropCallback(GLFWwindow* window, int count, const char** paths)
 {
     for(int i = 0; i < count; i++)
     {
         std::cout<<"Dropped file: "<<paths[i]<<std::endl;
+    }
+}
+
+void frameBufferCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
     }
 }
 
@@ -23,9 +38,13 @@ int main()
         return -1;
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     GLFWwindow* window = glfwCreateWindow(640, 480, "GLFW Example", NULL, NULL);
     if(!window)
-    {
+    {   
         std::cerr<<"Failed to create GLFW window."<<std::endl;
         glfwTerminate();
         return -1;
@@ -36,8 +55,27 @@ int main()
 
     glfwMakeContextCurrent(window);
 
+
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr<<"Failed to initialize GLAD."<<std::endl;
+        return -1;
+    }
+    
+
+    glViewport(0, 0, 640, 480);
+    glfwSetFramebufferSizeCallback(window, frameBufferCallback);
+
     while(!glfwWindowShouldClose(window))
     {
+        processInput(window);
+
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
