237ea154f78a8e6d12a4bfdff55f842d793ff6a6
Ritham Sheti <sethiritham@gmail.com>
2026-01-31 19:04:16 +0530

    feat : added image data writing logic

--- src/generator/png_generator.cpp
+++ src/generator/png_generator.cpp
@@ -42,10 +42,10 @@ bool Save(const char* filename, Image& edited_image)
     
     file.write("RITHAM", 6); //FILE SIGNATURE
     
-    write_little_endian_32(file, file_size);
-    write_little_endian_32(file, 0);
-    write_little_endian_32(file, data_offset);
-    write_little_endian_32(file, header_size);
+    write_little_endian_32(file, file_size); 
+    write_little_endian_32(file, 0); // reserved 
+    write_little_endian_32(file, data_offset); // where the pixel data starts
+    write_little_endian_32(file, header_size); 
 
     write_little_endian_32(file, edited_image.width);
     write_little_endian_32(file, -(int32_t)edited_image.height);
@@ -54,14 +54,41 @@ bool Save(const char* filename, Image& edited_image)
     file.put(1); file.put(0); //0x10
     file.put(24); file.put(0); //0x18
 
-    write_little_endian_32(file, 0);
-    write_little_endian_32(file, 0);
-    write_little_endian_32(file, 0);
-    write_little_endian_32(file, 0);    
-    write_little_endian_32(file, 0);
-    write_little_endian_32(file, 0);
+    write_little_endian_32(file, 0);// Compression (NONE)   
+    write_little_endian_32(file, 0);// Image Size (No compression, can be set to 0)
+    write_little_endian_32(file, 0);// X Pixels Per Meter (Unused, set to 0).
+    write_little_endian_32(file, 0);// Y Pixels Per Meter (Unused, set to 0).    
+    write_little_endian_32(file, 0);// Colors in Pallete (0 default)
+    write_little_endian_32(file, 0);// Important colours (0 = All)
 
     // IMAGE DATA
+    const std::vector<uint8_t>& src = edited_image.editedPixels;
+    int channels = edited_image.channels;
+
+    for(int y = 0; y < edited_image.height; y++)
+    {
+        for(int x = 0; x < edited_image.width; x++)
+        {
+            int index = (y* edited_image.width + x)*channels;
+            int r = src[index];
+            int g = src[index + 1];
+            int b = src[index + 2];
+
+            file.put(b);
+            file.put(g);
+            file.put(r);
+        }
+
+        for(int p = 0; p < padding_amount; p++)
+        {
+            file.put(0);
+        }
+    }
+
+    file.close();
+
+    std::cout<<"File writing done! "<<std::endl;
+    return true;
 
 }
 
